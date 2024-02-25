84cfaa3bb62fd7f765391e098298f601dde2fd7d
nilesh0700 <nileshthengde0700@gmail.com>
2024-02-25 11:55:58 +0530

    added text to state, and styled the text

--- src/Components/Meme.jsx
+++ src/Components/Meme.jsx
@@ -19,14 +19,43 @@ export default function Meme(){
            randomImage: url 
         }));
     }
+
+    function handleChange(){
+        const {name, value} = event.target;
+        setMeme(prevMeme => ({
+            ...prevMeme,
+            [name]: value
+        }))
+    }
     return(
         <main>
             <div className="form" >
-                <input type="text" placeholder="Top Text" className="form--input"/>
-                <input type="text" placeholder="Bottom text" className="form--input"/>
+                <input 
+                    type="text" 
+                    placeholder="Top Text" 
+                    className="form--input"
+                    name="topText"
+                    value={meme.topText}
+                    onChange={handleChange}    
+                />
+
+                <input 
+                    type="text" 
+                    placeholder="Bottom text" 
+                    className="form--input"
+                    name="bottomText"
+                    value={meme.bottomText}
+                    onChange={handleChange}    
+                />
                 <button className="form--button" onClick={getMemeImage}>Get a new meme image</button>
             </div>
+
+            <div className="meme">
                 <img src={meme.randomImage} className="meme--image" alt="" />
+                <h2 className="meme--text top">{meme.topText}</h2>
+                <h2 className="meme--text bottom">{meme.bottomText}</h2>
+
+            </div>
         </main>
     )
 }
